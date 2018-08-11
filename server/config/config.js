462347894ae35908013902890171e880f706357b
Christian <kisquian@gmail.com>
2018-08-11 12:00:03 -0300

    CRUD de categorias y productos

--- server/config/config.js
+++ server/config/config.js
@@ -9,7 +9,7 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev'
 // ======================
 // Vencimiento de token
 // ======================
-process.env.CADUCIDAD_TOKEN = 60 * 60 * 24 * 30;
+process.env.CADUCIDAD_TOKEN = '48h';
 // ======================
 // Secret o Seed de autenticación
 // ======================
