e609f33b11b55ca992fa7ec43dcc6f9c94033045
Christian <kisquian@gmail.com>
2018-08-10 22:07:48 -0300

    Google sign in

--- server/config/config.js
+++ server/config/config.js
@@ -1,20 +1,15 @@
 // ======================
 // Puerto
 // ======================
-
 process.env.PORT = process.env.PORT || 3000;
-
 // ======================
 // Entorno
 // ======================
-
 process.env.NODE_ENV = process.env.NODE_ENV || 'dev'
-
 // ======================
 // Vencimiento de token
 // ======================
 process.env.CADUCIDAD_TOKEN = 60 * 60 * 24 * 30;
-
 // ======================
 // Secret o Seed de autenticación
 // ======================
@@ -24,11 +19,13 @@ process.env.SEED = process.env.SEED || 'secret';
 // Base de datos
 // ======================
 let urlDB;
-
 if(process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/burguerQueen'
 } else {
     urlDB = process.env.MongoDB
 }
-
 process.env.URLDB = urlDB;
+// ======================
+// Google Client ID
+// ======================
+process.env.CLIENT_ID = process.env.CLIENT_ID || '667213072482-l5kh0l4t6ofue9aktgvg36m0afi2lq2u.apps.googleusercontent.com';
